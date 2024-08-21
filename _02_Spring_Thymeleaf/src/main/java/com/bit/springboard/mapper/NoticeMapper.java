c927945f4f565c2107543c64e4ab34632c1b5923
alohalo2 <wntjd7892@naver.com>
2024-08-21 21:00:15 +0900

    24.08.21 update

---
+++ _02_Spring_Thymeleaf/src/main/java/com/bit/springboard/mapper/NoticeMapper.java
@@ -0,0 +1,25 @@
+package com.bit.springboard.mapper;
+
+import com.bit.springboard.dto.BoardDto;
+import com.bit.springboard.dto.BoardFileDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface NoticeMapper {
+    void post(BoardDto boardDto);
+
+    void postFiles(List<BoardFileDto> boardFileDtoList);
+
+    BoardDto findById(int id);
+
+    void updateBoardCnt(int id);
+
+    int findTotalCnt(Map<String, String> searchMap);
+
+    List<BoardDto> findAll(Map<String, Object> paramMap);
+
+    List<BoardFileDto> findFilesById(int id);
+}
