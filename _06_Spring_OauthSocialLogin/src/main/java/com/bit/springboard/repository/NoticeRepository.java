b9d78aa26fac4abae323212beef8cda07166f268
alohalo2 <wntjd7892@naver.com>
2024-08-29 17:59:33 +0900

    24.08.29 update

---
+++ _06_Spring_OauthSocialLogin/src/main/java/com/bit/springboard/repository/NoticeRepository.java
@@ -0,0 +1,13 @@
+package com.bit.springboard.repository;
+
+import com.bit.springboard.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Map;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeRepositoryCustom {
+    Page<Notice> findByTitleContainingOrContentContainingOrMemberNicknameContaining(Pageable pageable, String searchKeyword, String searchKeyword1, String searchKeyword2);
+
+}
